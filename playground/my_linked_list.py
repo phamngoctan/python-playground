aeb3805c1bdcb234e9d8f23836fb974e0beb373e
Tan Pham <tan.pham@axonactive.com>
2020-11-23 14:35:14 +0700

    Add method reverse linkedlist

--- playground/my_linked_list.py
+++ playground/my_linked_list.py
@@ -1,30 +1,80 @@
 class Node:
   def __init__(self, data):
-    self.item = data
+    self.value = data
     self.next = None
-
+  
 class MyLinkedList:
   def __init__(self):
-    self.start_node = None
+    self.root = None
+    self.tail = None
+  
+  def add_to_front(self, data):
+    node = Node(data)
+    node.next = self.root
+    self.root = node
+    if self.tail is None:
+      self.tail = node
 
-  def traverse_list(self):
-    if self.start_node is None:
-      print("List has no element")
-      return
+  def add_to_tail(self, data):
+    node = Node(data)
+    if self.root is None:
+      self.root = node
+    
+    if self.tail is None:
+      self.tail = node
     else:
-      n = self.start_node
-      while n is not None:
-        print(n.item, " ")
-        n = n.next
+      self.tail.next = node
+      self.tail = node
 
-  def insert_at_start(self, data):
-    new_node = Node(data)
-    new_node.next = self.start_node
-    self.start_node = new_node
+  def reverse_from_root(self, tail = None):
+    return self.__reverse(self.root, tail)
+  
+  def __reverse(self, node, tail = None):
+    if node == self.root:
+      self.tail = node
 
-my_linked_list = MyLinkedList()
-my_linked_list.insert_at_start(3)
-my_linked_list.insert_at_start(4)
-my_linked_list.insert_at_start(5)
-my_linked_list.traverse_list()
+    if node is None:
+      self.root = tail
+      return tail
+    else:
+      next_node = node.next
+      node.next = tail
+      tail = node
+      return self.__reverse(next_node, tail)
+  
+  def print_all(self):
+    if self.root is None:
+      print("No item inside")
+    else:
+      cur = self.root
+      while cur is not None:
+        print(" " + str(cur.value))
+        cur = cur.next
 
+myLinkedList = MyLinkedList()
+# myLinkedList.add_to_front(3)
+# myLinkedList.add_to_front(2)
+# myLinkedList.add_to_front(1)
+# myLinkedList.add_to_front(0)
+print("try to print empty MyLinkedList")
+myLinkedList.print_all()
+# stdin = list(map(int, input().split()))
+# for i in stdin:
+#   myLinkedList.add_to_tail(i)
+print("====================")
+myLinkedList.add_to_tail(1)
+myLinkedList.add_to_tail(2)
+myLinkedList.add_to_tail(3)
+
+myLinkedList.print_all()
+# myLinkedList.reverse()
+myLinkedList.reverse_from_root()
+print("after reversed")
+myLinkedList.print_all()
+print("====================")
+print("after reversed and adding new item: 4")
+myLinkedList.add_to_tail(4)
+myLinkedList.print_all()
+myLinkedList.reverse_from_root()
+print("after reversed")
+myLinkedList.print_all()
