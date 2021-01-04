309efd60abae3aff36f1e42474ead2f09bdcd0d7
Tan Pham <tan.pham@axonactive.com>
2021-01-04 13:32:11 +0700

    Add python solution for min stack exercise

--- leetcode/min_stack.py
+++ leetcode/min_stack.py
@@ -4,18 +4,25 @@ class MinStack:
         """
         initialize your data structure here.
         """
-
+        self.internalStorage = []
     def push(self, x: int) -> None:
+        minVal = x
+        if len(self.internalStorage) > 0:
+            minVal = min(self.internalStorage[-1][1], x)
+        self.internalStorage.append([x, minVal])
 
     def pop(self) -> None:
+        self.internalStorage.pop()
 
     def top(self) -> int:
+        return self.internalStorage[-1][0]
 
     def getMin(self) -> int:
+        return self.internalStorage[-1][1]
 
 # Your MinStack object will be instantiated and called as such:
 obj = MinStack()
-obj.push(x)
+obj.push(-2)
 obj.pop()
 param_3 = obj.top()
 param_4 = obj.getMin()
