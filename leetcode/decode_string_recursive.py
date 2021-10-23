f985d39211848eb11889f2eda21583068200cb96
Tan Pham <pnt.dtqn1991@gmail.com>
2021-10-23 14:22:31 +0700

    Add solution for medium exercise - decode string - using recursive approach

---
+++ leetcode/decode_string_recursive.py
@@ -0,0 +1,32 @@
+class Solution:
+  def decodeString(self, s: str) -> str:
+    global i
+    i = 0
+    def helper(s):
+      global i
+      res = ""
+      curNum = 0
+      while i < len(s):
+        c = s[i]
+        i += 1
+        if c == '[':
+          sub = helper(s)
+          res = res + curNum * sub
+          curNum = 0 # reset the number after finishing the solving of sub problem
+        elif c == ']':
+          break # stop the loop for recursive call
+        elif c.isnumeric():
+          curNum = curNum*10 + int(c)
+        else:
+          res += c
+      return res
+    res = helper(s)
+    # print(f'{res}')
+    return res
+sol = Solution()
+assert sol.decodeString("3[a]2[bc]") == "aaabcbc"
+assert sol.decodeString("3[a2[c]]") == "accaccacc"
+assert sol.decodeString("2[abc]3[cd]ef") == "abcabccdcdcdef"
+assert sol.decodeString("abc3[cd]xyz") == "abccdcdcdxyz"
+assert sol.decodeString("3[z2[ab]]") == "zababzababzabab"
+assert sol.decodeString("2[z2[ab]]cbd") == "zababzababcbd"
