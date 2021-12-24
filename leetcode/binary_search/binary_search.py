763cd055a0c1606ca189138cc2cec110a7225d57
Tan Pham <pnt.dtqn1991@gmail.com>
2021-12-24 07:24:46 +0700

    Add more binary search technique to the library

--- leetcode/binary_search/binary_search.py
+++ leetcode/binary_search/binary_search.py
@@ -16,5 +16,35 @@ def binary_search(arr, target):
       right = mid - 1
   return -1
 
+def binary_search_rightMost(nums, target):
+  left, right = 0, len(nums) - 1
+  pos = None
+  while left <= right:
+    mid = left + (right - left) // 2
+    if (mid == right or target < nums[mid + 1]) and nums[mid] == target:
+      pos = mid
+      break
+    elif target < nums[mid]:
+      right = mid - 1
+    else:
+      left = mid + 1
+  # print(f'{pos}')
+  return pos if pos != None else -1
+
+def binary_search_rightMostForInsert(nums, target):
+  lo, hi = 0, len(nums) - 1
+  while lo < hi:
+    mid = (lo + hi + 1) // 2
+    if nums[mid] > target:
+      hi = mid - 1
+    else:
+      lo = mid
+  return lo
+
 assert binary_search([1,2,3,4,5,6,7], 5) == 4
 assert binary_search([], 10) == -1
+assert binary_search_rightMost([1,2,2,2,3,4,5,6,7], 2) == 3
+assert binary_search_rightMostForInsert([1,2,2,2,3,4,5,6,7], 2) == 3
+assert binary_search_rightMostForInsert([1,2,2,2,4,5,6,7], 3) == 3
+assert binary_search_rightMostForInsert([1,2,3,4,4,5,6,7], 3) == 2
+assert binary_search_rightMostForInsert([1,2,2,2,4,5,6,7], 3) == 3
