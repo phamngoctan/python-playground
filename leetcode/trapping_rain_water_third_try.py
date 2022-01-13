3831cd043b5cca9c1e89d90d6cd99adcff2eff2b
Tan Pham <pnt.dtqn1991@gmail.com>
2022-01-13 13:46:12 +0700

    Add one more solution for hard problem - trapping rain water II - using monotonic stack

--- leetcode/trapping_rain_water_third_try.py
+++ leetcode/trapping_rain_water_third_try.py
@@ -1,7 +1,7 @@
 from typing import List
 
 class Solution:
-  def trap(self, height: List[int]) -> int:
+  def trap_twoPointers(self, height: List[int]) -> int:
     maxWater = 0
     pl = 0
     pr = len(height) - 1
@@ -24,12 +24,28 @@ class Solution:
       maxWater += addedWater if addedWater > 0 else 0
       # print('--------')
     return maxWater
+  
+  def trap(self, height: List[int]) -> int:
+    """
+    Different view of the trapped water, instead of viewing vertically,
+    we view the problem in horizontally
+    """
+    ans, mStack = 0, []
+    for i in range(len(height)):
+      while mStack and height[i] > height[mStack[-1]]:
+        h = height[mStack.pop()]
+        if mStack:
+          minH = min(height[mStack[-1]], height[i])
+          ans += (minH - h)*(i - mStack[-1] - 1)
+      mStack.append(i)
+    return ans
 
 sol = Solution()
-print(sol.trap([0,1,0,2,1,0,1,3,2,1,2,1])) # 6
-print(sol.trap([4,2,0,3,2,5])) # 9
-print(sol.trap([9,8,7,6,5,4,3,2,1])) # 0
-print(sol.trap([4,2,3])) # 1
-print(sol.trap([4,2])) # 0
+assert sol.trap([0,1,0,2,1,0,1,3,2,1,2,1]) == 6
+sol.trap([4,2,0,3,5]) == 7
+sol.trap([4,2,0,3,2,5]) == 9
+sol.trap([9,8,7,6,5,4,3,2,1]) == 0
+sol.trap([4,2,3]) == 1
+sol.trap([4,2]) == 0
 
 
